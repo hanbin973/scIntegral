0f70b4eae91c7a50767933be8e84f3e32750d8ba
hanbin973 <hanbin973>
2020-11-22 20:46:57 +0900

    upload to pip

--- scintegral/utils.py
+++ scintegral/utils.py
@@ -2,11 +2,13 @@ import numpy as np
 import pandas as pd
 
 def marker_input_creator(marker_dict):
-	marker_onehot = pd.DataFrame(index=sum(list(marker_dict.values()),[]),
+	marker_onehot = pd.DataFrame(
+			index=sum(list(marker_dict.values()),[]),
 			columns=marker_dict.keys())
 	
 	for key, value in marker_dict.items():
 		marker_onehot.loc[value,key] = 1
+
 	marker_onehot.fillna(0, inplace=True)
 	marker_onehot['others'] = 0
 
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import setuptools
+
+setup(
+		name = 'scintegral',
+		version = '0.1.2',
+		author = 'Hanbin Lee',
+		author_email = 'hanbin973@gmail.com',
+		description = 'Semi-supervised scRNA-seq cell clasifier',
+		url = 'https://github.com/hanbin973/scIntegral',
+		packages=setuptools.find_packages(),
+		install_requires = ['numpy>=1.14', 'pandas>=1.1.0', 'torch>=1.6'],
+		classifiers=[
+			"Programming Language :: Python :: 3",
+			"Operating System :: OS Independent",
+			"Intended Audience :: Science/Research",
+			"Topic :: Scientific/Engineering",
+			"Topic :: Scientific/Engineering :: Bio-Informatics"
+		],
+		python_requires = '>=3.4',
+		)
