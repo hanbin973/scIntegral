1235a656cf383910e03927b4d6f0a989a3ae0545
hanbin973 <hanbin973>
2020-11-22 22:51:37 +0900

    readthedocs config attempt

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,11 @@ version = VERSION
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+		'sphinx.ext.viewcode',
 		'sphinx_rtd_theme',
 ]
+
+audodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
